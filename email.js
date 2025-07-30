28e5c30d86b941d6df88fd14ad7b874415207ed6
MigsGit <miguellgsp12345@gmail.com>
2025-07-30 11:28:31 +0800

    Email js | Get CDN & the email js configuration Create an Account Go to Login Email Js Add Service Connect Account Create Service Get the Config Id

---
+++ email.js
@@ -0,0 +1,8 @@
+// Your EmailJS settings - replace with your actual values
+const emailSettings = {
+    serviceId: 'service_e5mqpbu',
+    templateId: 'template_75c363g', 
+    publicKey: 'DfD3G7yL-gBoZRsFV'
+};
+
+console.log(emailSettings);
